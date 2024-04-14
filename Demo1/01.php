3525c9989c7f68fa1a25fda48b02233a66ed5763
Shako <shakoinbox@gmail.com>
2024-04-14 01:20:00 -0700

    AddedFeature

--- Demo1/01.php
+++ Demo1/01.php
@@ -49,12 +49,14 @@ echo $f;
 echo "\n--------- Scopes ----------\n";
 $a = 3;
 $b = 4;
-$c = ($a** 2 + $b** 2)**0.5;
+$c = ($a** 2 + $b** 2)**0.51;
 echo $c;
 
 echo "\n--------- usefulness ----------\n";
 var_dump($a);
 var_dump($f);
 var_dump($d1);
-$d2 = 12.05;
+$d2 = 13.05;
 var_dump($d2);
+echo 'Hello new branch';
+echo 'Hello next branch';
