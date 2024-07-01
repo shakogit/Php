0720be65a8f359fcd70f7e21d9edfeaeb73dec4b
Shako <shakoinbox@gmail.com>
2024-07-01 13:32:52 -0700

    MVC_1

---
+++ MVC/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <title>Document</title>
+</head>
+<body>
+<!-- Authorization forms -->
+  <form action="" method="get">
+    <label>Login</label>
+    <input type="text" placeholder="Enter Login"> 
+    <label>Password</label>
+    <input type="password" placeholder="Enter password">
+    <button type="button">Enter</button>
+
+    <p>You have not Accoun? <a href="indexRegister.php">Register Here</a></p>
+    
+  </form>
+</body>
+</html>
---
+++ MVC/indexRegister.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <title>Document</title>
+</head>
+<body>
+<!-- Authorization forms -->
+  <form action="" method="get">
+    <label>Name</label>
+    <input type="text" placeholder="Enter your name"> 
+    <label>Email</label>
+    <input type="text" placeholder="Enter your Email"> 
+    
+    <label>Profile Image</label>
+    <input type="file">
+
+    <label>password</label>
+    <input type="password" placeholder="Enter password">
+    <label>Repeat Password</label>
+    <input type="password" placeholder="New password">
+    <button type="button">Enter</button>
+
+    <p>You have Registered <a href="index.php">Authoritaize</a></p>
+    
+  </form>
+</body>
+</html>
